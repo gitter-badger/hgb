9ea376601fd5d88a7686365b8c97b49ceab475f1
lukasberglund <lukastberglund@gmail.com>
2019-11-14 10:13:07 -0600

    small syntax changes

--- data.hs
+++ data.hs
@@ -9,55 +9,56 @@ data Token = Symbol String
 
 lexBasic :: String -> Symbol
 
-lexBasic "=/=" = NEQ
--- lexBasic "->" = ITER_UPTO
--- lexBasic ">=" = GEQ
--- lexBasic "<=" = LEQ
--- lexBasic "==" = EQ
-lexBasic "." = DOT
-lexBasic "[" = LBRACKET
-lexBasic "]" = RBRACKET
-lexBasic "(" = LPAREN
-lexBasic ")" = RPAREN
-lexBasic "{" = LBRACE
-lexBasic "}" = RBRACE
-lexBasic ":" = TYPE_DELIM
-lexBasic "," = VAL_DELIM
--- lexBasic "=" = ASSIGN
--- lexBasic ">" = GT
--- lexBasic "<" = LT
-lexBasic "+" = PLUS
-lexBasic "-" = MINUS
-lexBasic "*" = TIMES
-lexBasic "/" = DIV
-lexBasic "%" = MOD
-lexBasic "\'" = CHAR_BOUND
-lexBasic "\"" = STR_BOUND
-lexBasic "!" = EXPR_END
-lexBasic "in" = IN
-lexBasic "and" = AND
-lexBasic "not" = NOT
-lexBasic "or" = OR
-lexBasic "for" = FOR
-lexBasic "while" = WHILE
-lexBasic "if" = IF
-lexBasic "else" = ELSE
-lexBasic "elseif" = ELSEIF
-lexBasic "fun" = FUNC
-lexBasic "return" = RETURN
-lexBasic "int8" = TYPE
-lexBasic "int16" = TYPE
-lexBasic "int32" = TYPE
-lexBasic "int64" = TYPE
-lexBasic "int" = TYPE
-lexBasic "uint8" = TYPE
-lexBasic "uint16" = TYPE
-lexBasic "uint32" = TYPE
-lexBasic "uint64" = TYPE
-lexBasic "uint" = TYPE
-lexBasic "char" = TYPE
-lexBasic "void" = TYPE
-lexBasic s = NAME
+lexBasic word = case word of
+                "=/=" -> NEQ
+                "->" -> ITER_UPTO
+                ">=" -> GEQ
+                "<=" -> LEQ
+                "==" -> EQ
+                "." -> DOT
+                "[" -> LBRACKET
+                "]" -> RBRACKET
+                "(" -> LPAREN
+                ")" -> RPAREN
+                "{" -> LBRACE
+                "}" -> RBRACE
+                ":" -> TYPE_DELIM
+                "," -> VAL_DELIM
+                "=" -> ASSIGN
+                ">" -> GT
+                "<" -> LT
+                "+" -> PLUS
+                "-" -> MINUS
+                "*" -> TIMES
+                "/" -> DIV
+                "%" -> MOD
+                "\'" -> CHAR_BOUND
+                "\"" -> STR_BOUND
+                "!" -> EXPR_END
+                "in" -> IN
+                "and" -> AND
+                "not" -> NOT
+                "or" -> OR
+                "for" -> FOR
+                "while" -> WHILE
+                "if" -> IF
+                "else" -> ELSE
+                "elseif" -> ELSEIF
+                "fun" -> FUNC
+                "return" -> RETURN
+                "int8" -> TYPE
+                "int16" -> TYPE
+                "int32" -> TYPE
+                "int64" -> TYPE
+                "int" -> TYPE
+                "uint8" -> TYPE
+                "uint16" -> TYPE
+                "uint32" -> TYPE
+                "uint64" -> TYPE
+                "uint" -> TYPE
+                "char" -> TYPE
+                "void" -> TYPE
+                s -> NAME
 
 isWhiteSpace :: Char -> Bool
 isWhiteSpace c = elem c " \t\n"
