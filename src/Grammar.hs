0cde1d92b5cbece3153c8195877dbb24422a52a7
lukasberglund <lukastberglund@gmail.com>
2019-11-17 11:37:44 -0600

    removed merge conflict

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -1,10 +1,4 @@
 module Grammar where
-<<<<<<< HEAD
 
-data Expressions = Expr | Expr Expressions
-data Expr = Expr Op Expr | Paren Expr Paren
-=======
---
 -- data Expressions = Expr | Expr Expressions
 -- data Expr = Expr Op Expr | Paren Expr Paren
->>>>>>> first time no compiler error
