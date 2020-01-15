e92b588e99c930ebf4858447c970832e107fc783
hafiz <20735482+ayazhafiz@users.noreply.github.com>
2020-01-15 15:46:34 -0800

    Minor fixups to make hlint happy (#17)
    Ran hlint through and fixed minor niceties.

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -14,7 +14,7 @@ lex = lex' 0
 lex' :: Int -> String -> [Token]
 lex' _ "" = []
 lex' i all@(x:xs)
-  | isSpace (x) = lex' (i + 1) xs
+  | isSpace x = lex' (i + 1) xs
   | [x] == Builtin.KeywordStrBound = lexStr i all
   | isDigit x = lexNumber i all
   | isAlpha x = lexAlphaKeyword i all
@@ -23,49 +23,49 @@ lex' i all@(x:xs)
 
 lexStr :: Int -> String -> [Token]
 lexStr i (delim:xs) =
-  (Token Symbol.StrBound [delim] i contentStart) : contentToken : postContent
+  Token Symbol.StrBound [delim] i start : contentToken : postContent
     -- Parse content after the opening string delimiter, returning as much as possible of
     -- 1. String content
     -- 2. String closing delimiter
     -- 3. Tokens after string
   where
     (contentStr, postContentStr) = break (== delim) xs
-    contentStart = i + 1
-    contentEnd = contentStart + length contentStr
-    contentToken = (Token Symbol.String contentStr contentStart contentEnd)
+    start = i + 1
+    end = start + length contentStr
+    contentToken = Token Symbol.String contentStr start end
     postContent =
       case postContentStr of
         (endDelim:remainder) ->
-          (Token Symbol.StrBound [endDelim] contentEnd (contentEnd + 1))
-            : lex' (contentEnd + 1) remainder
-        otherwise -> []
+          Token Symbol.StrBound [endDelim] end (end + 1)
+            : lex' (end + 1) remainder
+        _ -> []
 
 lexNumber :: Int -> String -> [Token]
 lexNumber i str =
-  (Token Symbol.Number numStr i end) : lex' end remainder
+  Token Symbol.Number numStr i end : lex' end remainder
   where
     (numStr, remainder) = span isDigit str
     end = i + length numStr
 
 lexAlphaKeyword :: Int -> String -> [Token]
 lexAlphaKeyword i str =
-  (Token symbol keyword i end) : lex' end remainder
+  Token symbol keyword i end : lex' end remainder
   where
     (keyword, remainder) = span isAlphaNum str
     end = i + length keyword
     symbol = case keywordToSymbol keyword of
       Just s -> s
-      otherwise -> Symbol.Name
+      _ -> Symbol.Name
 
 lexOperatorOrPunctuation :: Int -> String -> [Token]
 lexOperatorOrPunctuation i str =
-  (Token symbol keyword i end) : lex' end remainder
+  Token symbol keyword i end : lex' end remainder
   where
     (keyword, remainder) = span (isAnyOf [isSymbol, isPunctuation]) str
     end = i + length keyword
     symbol = case keywordToSymbol keyword of
       Just s -> s
-      otherwise -> Symbol.Invalid
+      _ -> Symbol.Invalid
 
 isAnyOf :: [c -> Bool] -> c -> Bool
-isAnyOf p c = or $ map ($c) p
+isAnyOf p c = any ($c) p
