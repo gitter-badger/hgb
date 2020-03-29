c642f5832ee3c019314209312669e16ad8d2d8c9
lukasberglund <lukastberglund@gmail.com>
2020-03-29 10:12:29 +0200

    wrote evalInt

---
+++ src/Eval.hs
@@ -0,0 +1,14 @@
+module Eval where
+
+import Grammar
+
+evalInt :: Expression -> Int
+evalInt (Number n) = n
+evalInt (Call function args) =
+  case function of
+    "+" -> a + b
+    "-" -> a - b
+    "*" -> a * b
+    "/" -> a `div` b
+    "%" -> a `mod` b
+  where [a, b] = map evalInt args
