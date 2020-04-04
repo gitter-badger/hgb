74a8d8842ab7ffe3c18d62242e71534010c83386
hafiz <20735482+ayazhafiz@users.noreply.github.com>
2020-04-03 23:46:53 -0700

    Humanize lexer tests (#48)
    Closes #47

--- test/Token/TokenSpec.hs
+++ test/Token/TokenSpec.hs
@@ -11,8 +11,7 @@ import Utils (Span(..))
 spec :: Spec
 spec = do
   it "should show tokens correctly" $
-    show (Token Symbol.String "str" (Span 0 3)) `shouldBe`
-    "Token {symbol = String, content = \"str\", sourceSpan = 0:3}"
+    show (Token Symbol.String "str" (Span 0 3)) `shouldBe` "String(str)@0:3"
   it "should access token data fields correctly" $ do
     let t = Token Symbol.Name "zifah" (Span 5 10)
     (symbol t, content t, sourceSpan t) `shouldBe`
